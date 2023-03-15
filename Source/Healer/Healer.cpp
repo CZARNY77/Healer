8150b05a44e904bd51fd8276dd9f57920f8d4a81
CZARNY77 <60876248+CZARNY77@users.noreply.github.com>
2023-03-15 14:14:45 +0100

    Initial commit

---
+++ Source/Healer/Healer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Healer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Healer, "Healer" );
